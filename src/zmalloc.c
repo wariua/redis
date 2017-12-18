b4b473aca0e9a88ce5590fa44b878b4142d7bbfe
Seonghun Lim <wariua@gmail.com>
2017-12-19 08:59:53 +0900

    misc: fix memory accounting
    Two accounting macros calculate real size of memory blocks, and then just ignore it.
    The incorrect use of variable first appeared in c7b46a47, and survived 6eb51bf1.

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -73,13 +73,13 @@ void zlibc_free(void *ptr) {
 #define update_zmalloc_stat_alloc(__n) do { \
     size_t _n = (__n); \
     if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
-    atomicIncr(used_memory,__n); \
+    atomicIncr(used_memory,_n); \
 } while(0)
 
 #define update_zmalloc_stat_free(__n) do { \
     size_t _n = (__n); \
     if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
-    atomicDecr(used_memory,__n); \
+    atomicDecr(used_memory,_n); \
 } while(0)
 
 static size_t used_memory = 0;
