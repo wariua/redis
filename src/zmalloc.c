768c191f92c064d7e7da2b9616896336229d3c1c
Seonghun Lim <wariua@gmail.com>
2017-12-19 15:01:37 +0900

    misc: Remove unused variables (see 2a51bac4)

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -83,7 +83,6 @@ void zlibc_free(void *ptr) {
 } while(0)
 
 static size_t used_memory = 0;
-pthread_mutex_t used_memory_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void zmalloc_default_oom(size_t size) {
     fprintf(stderr, "zmalloc: Out of memory trying to allocate %zu bytes\n",
