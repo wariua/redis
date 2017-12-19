7148d3e39dbd97f6b1331f247fad61d9f8b5d514
Seonghun Lim <wariua@gmail.com>
2017-12-19 13:57:00 +0900

    elist: Added new list implementation w/o malloc
    This is a simplified version of ~linux/include/linux/list.h.

---
+++ src/elist.h
@@ -0,0 +1,112 @@
+/* elist.h - A generic doubly linked list implementation
+ *
+ * Copyright (c) 2017, Seonghun Lim <wariua at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ELIST_H__
+#define __ELIST_H__
+
+#include <stddef.h>
+
+/* Circular doubly linked lists with embedded nodes. */
+
+typedef struct elNode {
+    struct elNode *prev;
+    struct elNode *next;
+} elNode;
+
+typedef struct elList {
+    elNode head;
+    unsigned long len;
+} elList;
+
+#define elFirst(list)   ((list)->head.next)
+#define elLast(list)	((list)->head.prev)
+
+#define container_of(ptr, member, type) (void *)((ptrdiff_t)(ptr) - offsetof(type, member))
+#define elNodeValue(node, member, type) container_of(node, member, type)
+
+#define elForEach(list, node)  \
+    for (node = elFirst(list); node != &(list)->head; node = node->next)
+
+#define elForEachSafe(list, node, next)    \
+    for (node = elFirst(list); node != &(list)->head && (next = node->next, 1); node = next)
+
+static inline void elInitNode(elNode *n)
+{
+    n->prev = n;
+    n->next = n;
+}
+
+static inline void elInitList(elList *l)
+{
+    elInitNode(&l->head);
+    l->len = 0;
+}
+
+static inline void elInsertBefore(elNode *n, elNode *old)
+{
+    n->prev = old->prev;
+    n->next = old;
+    old->prev->next = n;
+    old->prev = n;
+}
+
+static inline void elAppendAfter(elNode *n, elNode *old)
+{
+    n->prev = old;
+    n->next = old->next;
+    old->next->prev = n;
+    old->next = n;
+}
+
+static inline void elDel(elNode *n)
+{
+    n->prev->next = n->next;
+    n->next->prev = n->prev;
+}
+
+static inline void elAddNodeHead(elList *l, elNode *n)
+{
+    elAppendAfter(&l->head, n);
+    l->len++;
+}
+
+static inline void elAddNodeTail(elList *l, elNode *n)
+{
+    elInsertBefore(&l->head, n);
+    l->len++;
+}
+
+static inline void elDelNode(elList *l, elNode *n)
+{
+    elDel(n);
+    l->len--;
+}
+
+#endif /* __ELIST_H__ */
